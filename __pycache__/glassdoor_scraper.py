b74a5a3f0bb68662c17abf5b4593508740dbf21d
Jagan Nathan <jagannathan@Jagans-MacBook-Air.local>
2020-04-10 04:01:24 +0530

    uploaded scraper and run code

---
+++ __pycache__/glassdoor_scraper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 02:24:11 2020
+
+@author: jagannathan
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "/Users/jagannathan/Documents/ds_salary_proj/chromedriver"
+
+df = gs.
