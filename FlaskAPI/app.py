7114c77bc24655f9cf481f86301d270a1686f476
Jagannathan J <56400876+Jagan-droid@users.noreply.github.com>
2020-04-18 20:57:28 +0530

    Update app.py

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -2,9 +2,8 @@ import flask
 from flask import Flask, jsonify, request
 import json
 from data_input import data_in
-
-app = Flask(__name__)
-@app.route('/predict', methods=['GET'])
+import numpy as np
+import pickle
 
 def load_models():
     file_name = "models/model_file.p"
@@ -13,12 +12,16 @@ def load_models():
         model = data['model']
     return model
 
+app = Flask(__name__)
+@app.route('/predict', methods=['GET'])
+
 def predict():
     # stub input features
-    x = 5.963
+    x = np.array(data_in).reshape(1,-1)
+    
     # load model
     model = load_models()
-    prediction = model.predict([[x]])[0]
+    prediction = model.predict(x)[0]
     response = json.dumps({'response': prediction})
     return response, 200
 
